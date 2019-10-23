e161b22b77f782ab33c1a7660d9743f9593eaed4
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-23 04:56:41 +0100

    gave baby a name

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -4,5 +4,9 @@ const Pet = require("../src/my-pet");
 describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
-    }) ;
+    });
+    const baby = new Pet('Fido');
+    it('gives the pet a name', () => {
+        expect(baby.name).toBe('Fido');
+    })
 });
