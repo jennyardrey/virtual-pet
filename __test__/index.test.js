c30d6dd56e99953ba725cf1656572b829ba8a271
Jenny Ardrey <jenny@ardrey.co.uk>
2019-10-23 15:30:43 +0100

    gave Pet a growup method

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,5 +8,20 @@ describe('constructor', () => {
     const baby = new Pet('Fido');
     it('gives the pet a name', () => {
         expect(baby.name).toBe('Fido');
-    })
+    });
+});
+
+describe('constructor', () => {
+    const baby = new Pet('Fido');
+    it('has an initial age of 0', () => {
+        expect(baby.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increases age in increments of 1', () => {
+        const baby = new Pet('Fido');
+        baby.growUp();
+        expect(baby.age).toEqual(1);
+    });
 });
